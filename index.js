7ea689eeb47bc4ac842aa0a66532fcd76c30f4ef
Andres Mejias <aemabit@gmail.com>
2020-01-14 22:58:05 -0500

    link to

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const amazonJob = require("./amazonJob");
 
+let MacBookPrLink = 'https://www.amazon.com/dp/B081FV1Y57/ref=fs_a_mb_3?th=1'
+
 const amazon = async () => {
   await amazonJob.initialize();
-  let details = await amazonJob.getProductDetails('https://www.amazon.com/POGT-Sleeve-Blazer-Cardigan-Mustard/dp/B01M7S9QBJ?th=1&psc=1')
+  let details = await amazonJob.getProductDetails(MacBookPrLink)
 
   debugger;
 };
